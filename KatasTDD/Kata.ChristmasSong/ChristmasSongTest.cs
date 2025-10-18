7bd9ca5f858b03db6b11ed90167433031e7417bc
Michael Andrés Lozano Sánchez <Michael.Lozano@sinco.com.co>
2025-10-18 18:27:47 -0500

    🟥 Creacion del primer test para validar la primera linea de la primera estrofa

---
+++ KatasTDD/Kata.ChristmasSong/ChristmasSongTest.cs
@@ -0,0 +1,29 @@
+﻿
+using AwesomeAssertions;
+
+namespace ChrismasSong;
+
+public class ChristmasSongTest
+{
+    //- [ ] La primera linea de la primera estrofa, debe tener el siguiente contenido: On the first day of Christmas
+    [Fact]
+    public void test()
+    {
+        //Arrange
+        var song = new Song();
+        
+        //Act
+        var firstLine = song.GetStropheFirstLine();
+
+        //Assert
+        firstLine.Should().Be("On the first day of Christmas");
+    }
+}
+
+public class Song
+{
+    public object GetStropheFirstLine()
+    {
+        throw new NotImplementedException();
+    }
+}
