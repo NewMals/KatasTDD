433dfa62598e53fbc507ede89a2bea414252e313
NewMals <newmals313@gmail.com>
2025-10-19 12:02:33 -0500

    🟥 Fail, Test si el primer numero es 4 y el segundo numero es 8, debe devolve el valor minimo 4

--- Katas/Kata.CalculateStats/CalculateStatsTest.cs
+++ Katas/Kata.CalculateStats/CalculateStatsTest.cs
@@ -5,14 +5,28 @@ namespace Kata.CalculateStats;
 public class CalculateStatsTest
 {
     [Fact]
-    public void Test1()
+    public void Si_Secuencia_Es_Vacia_Debe_Devolver_Excepcion()
     {
         //Arrange
         var calculateStats = () => new CalculateStats(null); 
-        var message = "Secuencia vacia";
+        const string message = "Secuencia vacia";
         
         //Asserts
-        calculateStats.Should().Throw<Exception>();
+        calculateStats.Should().Throw<Exception>().WithMessage(message);
+    }
+    
+    [Fact]
+    public void S_Primer_Numero_Cuatro_El_Segundo_Numero_Ocho_Debe_Devolve_Valor_Minimo_Cuatro()
+    {
+        //Arrange
+        var sequence = new List<int>(){4, 8};
+        var calculateStats = new CalculateStats(sequence); 
+        
+        //Act
+        var valueMin = calculateStats.GetValueMin();
+        
+        //Asserts
+        valueMin.Should().Be(4);
     }
 }
 
@@ -22,5 +36,10 @@ public class CalculateStats
     {
         if (sequence == null || sequence.Count == 0)
             throw new Exception("Secuencia vacia");
+    }
+
+    public object GetValueMin()
+    {
+        throw new NotImplementedException();
     }
 }
