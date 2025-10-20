682047e38314e2e467a82f6fdf60ee949c408202
NewMals <newmals313@gmail.com>
2025-10-19 20:47:16 -0500

    🟦 Refactor, separacion de archivos y buenas practicas

---
+++ Katas/Kata.CalculateStats/CalculateStats.cs
@@ -0,0 +1,28 @@
+namespace Kata.CalculateStats;
+
+public class CalculateStats
+{
+    private List<int> Sequence { get; set; }
+    public CalculateStats(List<int>? sequence)
+    {
+        if (sequence == null || sequence.Count == 0)
+            throw new Exception("Secuencia vacia");
+        
+        Sequence = sequence;
+    }
+
+    public int GetValueMin() => Sequence.Min();
+
+    public int GetValueMax() => Sequence.Max();
+
+    public double GetValueAverage() => Math.Round(Sequence.Average());
+
+    public int GetElements() => Sequence.Count;
+
+    public string GetStas() =>
+        $"Valor minimo: {GetValueMin()}\n\n" +
+        $"Valor maximo: {GetValueMax()}\n\n" +
+        $"Cantidad de elementos: {GetElements()}\n\n" +
+        $"Valor promedio: {GetValueAverage()}";
+    
+}
--- Katas/Kata.CalculateStats/CalculateStatsTest.cs
+++ Katas/Kata.CalculateStats/CalculateStatsTest.cs
@@ -90,30 +90,3 @@ public class CalculateStatsTest
         getStats.Should().Be(statsExpected);
     }
 }
-
-public class CalculateStats
-{
-    private List<int> Sequence { get; set; }
-    public CalculateStats(List<int>? sequence)
-    {
-        if (sequence == null || sequence.Count == 0)
-            throw new Exception("Secuencia vacia");
-        
-        Sequence = sequence;
-    }
-
-    public int GetValueMin() => Sequence.Min();
-
-    public int GetValueMax() => Sequence.Max();
-
-    public double GetValueAverage() => Math.Round(Sequence.Average());
-
-    public int GetElements() => Sequence.Count;
-
-    public string GetStas() =>
-        $"Valor minimo: {GetValueMin()}\n\n" +
-        $"Valor maximo: {GetValueMax()}\n\n" +
-        $"Cantidad de elementos: {GetElements()}\n\n" +
-        $"Valor promedio: {GetValueAverage()}";
-    
-}
