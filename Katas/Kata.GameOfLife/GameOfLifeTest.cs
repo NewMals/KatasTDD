7819b757d3e8919165dc40e099f100380d2bd35a
NewMals <newmals313@gmail.com>
2025-10-27 12:31:48 -0500

    Fail, primer tes para confirmar si una celula viva sin vecinos debe morir

---
+++ Katas/Kata.GameOfLife/GameOfLifeTest.cs
@@ -0,0 +1,33 @@
+﻿using AwesomeAssertions;
+
+namespace Kata.GameOfLife;
+
+public class GameOfLifeTest
+{
+    [Fact]
+    public void Si_UnaCelulaVivaSinVecinos_Debe_Morir()
+    {
+        var board = new[,] { { true } };
+        var game = new GameOfLife(board);
+        
+        game.NextGen();
+        
+        game.Board[0,0].Should().BeFalse();
+    }
+}
+
+public class GameOfLife
+{
+    public bool[,] Board { get; private set; }
+    
+    public GameOfLife(bool[,] board)
+    {
+        Board = board;
+    }
+
+
+    public void NextGen()
+    {
+        throw new NotImplementedException();
+    }
+};
