8a07b095d43ada96324e51052c30b2b19c37ac5d
NewMals <newmals313@gmail.com>
2025-11-02 17:21:49 -0500

    Fail, primer test para validar el numero arabico 1

---
+++ Katas/Kata.RomanNumerals/RomanNumeralsTest.cs
@@ -0,0 +1,24 @@
+﻿using AwesomeAssertions;
+
+namespace Kata.RomanNumerals;
+
+public class RomanNumeralsTest
+{
+    [Fact]
+    public void Si_ElNumeroArabicoEs1_Debe_RetornarI()
+    {
+        var romanNumeral = new RomanNumeral();
+
+        var roman = romanNumeral.Convert(1);
+
+        roman.Should().Be("I");
+    }
+}
+
+public class RomanNumeral
+{
+    public String Convert(int i)
+    {
+        throw new NotImplementedException();
+    }
+}
