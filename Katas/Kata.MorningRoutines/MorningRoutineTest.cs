bf88dc3184d8fe365d89e1713d3cb13522969672
NewMals <newmals313@gmail.com>
2025-11-03 21:40:58 -0500

    Refactor, mensaje devuelto mediante una excepcion

--- Katas/Kata.MorningRoutines/MorningRoutineTest.cs
+++ Katas/Kata.MorningRoutines/MorningRoutineTest.cs
@@ -8,7 +8,7 @@ public class MorningRoutinesTest
     public void Si_LaHoraInicialEsEntre_0600_A_0659_Debe_MostrarActividad_HacerEjercicio()
     {
         var expectedRoutine = "Hacer ejercicio";
-        var routineMorning = new MorningRoutine(new TimeSpan(6,0,0));
+        var routineMorning = new MorningRoutine(new TimeSpan(6, 0, 0));
 
         var activity = routineMorning.GetActivity();
 
@@ -19,29 +19,29 @@ public class MorningRoutinesTest
     public void Si_LaHoraInicialEsEntre_0700_A_0759_Debe_MostrarActividad_LeerYEstudiar()
     {
         var expectedRoutine = "Leer y estudiar";
-        var routineMorning = new MorningRoutine(new TimeSpan(7,0,0));
+        var routineMorning = new MorningRoutine(new TimeSpan(7, 0, 0));
 
         var activity = routineMorning.GetActivity();
 
         activity.Should().Be(expectedRoutine);
     }
-    
+
     [Fact]
     public void Si_LaHoraEstaEntre_0800_A_0859_Debe_MostrarActividad_Desayunar()
     {
         var expectedRoutine = "Desayunar";
-        var routineMorning = new MorningRoutine(new TimeSpan(8,0,0));
+        var routineMorning = new MorningRoutine(new TimeSpan(8, 0, 0));
 
         var activity = routineMorning.GetActivity();
 
         activity.Should().Be(expectedRoutine);
     }
-    
+
     [Fact]
     public void Si_LaHoraEstaEntre_0900_A_0559_Debe_MostrarActividad_SinActividad()
     {
         var expectedRoutine = "Sin actividad";
-        var routineMorning = new MorningRoutine(new TimeSpan(9,0,0));
+        var routineMorning = new MorningRoutine(new TimeSpan(9, 0, 0));
 
         var activity = routineMorning.GetActivity();
 
@@ -51,11 +51,11 @@ public class MorningRoutinesTest
     [Fact]
     public void Si_QuieroAgregarLaActividad_Leer_Entre_0700_A_0729_Debe_MostrarUnMensajeIndicando_LaActividadQueExiste()
     {
-        var routineMorning = new MorningRoutine(new TimeSpan(7,20,15));
-        
-        var activity = routineMorning.AddRoutine("Leer", new TimeSpan(7,0,0), new TimeSpan(7,29,59));
-        
-        activity.Should().Be("Ya existe una rutina para el horario de 07:00 A 07:59");
+        var routineMorning = new MorningRoutine(new TimeSpan(7, 20, 15));
+
+        var activity = () => routineMorning.AddRoutine("Leer", new TimeSpan(7, 0, 0), new TimeSpan(7, 29, 59));
+
+        activity.Should().ThrowExactly<Exception>().WithMessage("Para el horario de 07:00:00 a 07:29:59 existe la rutina Leer y estudiar");
     }
 }
 
@@ -64,18 +64,30 @@ public class MorningRoutine(TimeSpan currentTime)
     private const string SinActividad = "Sin actividad";
     private TimeSpan CurrentTime { get; } = currentTime;
 
-    public string GetActivity() => 
+    public string GetActivity() =>
         FindFirstRoutine()?.Name ?? SinActividad;
-    
-    public string AddRoutine(string name, TimeSpan startTime, TimeSpan endTime)
+
+    public void AddRoutine(string name, TimeSpan startTime, TimeSpan endTime)
     {
-        return "Ya existe una rutina para el horario de 07:00 A 07:59";
+        var activity = FindFirstRoutine(startTime, endTime);
+
+        if (activity is not null)
+            throw new Exception(
+                $"Para el horario de {startTime.ToString("c")} a {endTime.ToString("c")} existe la rutina {activity.Name}"
+            );
     }
 
     private Activity? FindFirstRoutine() =>
         Activity
             .Activities()
-            .FirstOrDefault(firstActivity => firstActivity.StartTime <= CurrentTime &&  firstActivity.EndTime >= CurrentTime);
+            .FirstOrDefault(firstActivity =>
+                firstActivity.StartTime <= CurrentTime && firstActivity.EndTime >= CurrentTime);
+
+
+    private Activity? FindFirstRoutine(TimeSpan startTime, TimeSpan endTime) =>
+        Activity
+            .Activities()
+            .FirstOrDefault(firstActivity => firstActivity.StartTime <= startTime && firstActivity.EndTime >= endTime);
 }
 
 public class Activity
@@ -106,19 +118,4 @@ public class Activity
             EndTime = new TimeSpan(8, 59, 59)
         }
     ];
-
-    public static void AddActivity(string name, TimeSpan startTime, TimeSpan endTime)
-    {
-        Activities().Add(new Activity
-        {
-            Name = name,
-            StartTime = startTime,
-            EndTime = endTime
-        });
-    }
-
-    private static void UpdateActivity(Activity activity)
-    {
-        Activities().Remove(activity);
-    }
 }
