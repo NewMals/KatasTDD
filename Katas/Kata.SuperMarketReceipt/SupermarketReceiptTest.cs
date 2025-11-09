ab57ce2d7bee740c3f39f8cc3eab7898e1a17ab9
NewMals <newmals313@gmail.com>
2025-11-09 13:54:41 -0500

    Fail, un nuevo caso de borde "Arveja"

--- Katas/Kata.SuperMarketReceipt/SupermarketReceiptTest.cs
+++ Katas/Kata.SuperMarketReceipt/SupermarketReceiptTest.cs
@@ -7,11 +7,11 @@ public class SupermarketReceiptTest
 {
     [Theory]
     [InlineData("Cepillo", 0.99)]
-    [InlineData("Manzanas", 1.99)]
+    [InlineData("Manzana", 1.99)]
     [InlineData("Arroz", 2.49)]
     [InlineData("TuboPastaDientes", 1.79)]
     [InlineData("TomateCherry", 0.69)]
-    public void Si_CompraSoloUnArticulo_ElPrecioTotalDelRecibo_Debe_SerElPrecioDelProducto(string product,
+    public void Si_CompraSoloUnProducto_ElPrecioTotalDelRecibo_Debe_SerElPrecioDelProducto(string product,
         decimal totalPrice)
     {
         var receipt = new SupermarketReceipt();
@@ -22,13 +22,24 @@ public class SupermarketReceiptTest
     }
 
     [Fact]
-    public void Si_NoExisteUnArticulo_Debe_MostrarUnaExcepcion()
+    public void Si_NoExisteElProductoPapa_Debe_MostrarUnaExcepcion()
     {
         var receipt = new SupermarketReceipt();
         
-        var exception = () => receipt.AddProduct("Papas");
+        var exception = () => receipt.AddProduct("Papa");
         
         exception.Should().Throw<Exception>();
+    }
+    
+    [Fact]
+    public void Si_NoExisteElProductoArveja_Debe_MostrarUnaExcepcion()
+    {
+        var product = "Arveja";
+        var receipt = new SupermarketReceipt();
+        
+        var exception = () => receipt.AddProduct(product);
+        
+        exception.Should().Throw<Exception>().WithMessage($"El producto {product} no existe");
     }
 }
 
@@ -37,7 +48,7 @@ public class SupermarketReceipt
     private string _product;
     public void AddProduct(string product)
     {
-        if (product == "Papas")
+        if (product == "Papa")
         {
             throw new Exception();
         }
@@ -50,10 +61,13 @@ public class SupermarketReceipt
         return _product switch
         {
             "Cepillo" => 0.99m,
-            "Manzanas" => 1.99m,
+            "Manzana" => 1.99m,
             "Arroz" => 2.49m,
             "TuboPastaDientes" => 1.79m,
             _ => 0.69m
         };
     }
 }
+
+
+
