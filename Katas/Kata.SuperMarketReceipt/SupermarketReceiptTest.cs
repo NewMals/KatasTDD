1a6119c4a9cdbf66e7f4c55b139448970170a5a5
NewMals <newmals313@gmail.com>
2025-11-09 23:07:39 -0500

    Success, se crean nuevos metodos para poder obtener los descuentos de los productos

--- Katas/Kata.SuperMarketReceipt/SupermarketReceiptTest.cs
+++ Katas/Kata.SuperMarketReceipt/SupermarketReceiptTest.cs
@@ -57,6 +57,7 @@ public class SupermarketReceiptTest
     public void Si_CompraSoloDosCepillosElTerceroEsGratisPorMotivoDeDescuento_PorLoTantoElPrecioTotal_Debe_Ser_1_98()
     {
         var receipt = new SupermarketReceipt();
+        receipt.AddDiscounts(["Cepillo"]);
         
         receipt.AddProductToCar("Cepillo");
         receipt.AddProductToCar("Cepillo");
@@ -69,6 +70,7 @@ public class SupermarketReceiptTest
     public void Si_Compra1KiloDeManzanas_Recibe20PorcientoDeDescuento_PorLoTantoElPrecioTotal_Debe_Ser_1_592()
     {
         var receipt = new SupermarketReceipt();
+        receipt.AddDiscounts(["Manzana"]);
         
         receipt.AddProductToCar("Manzana");
         
@@ -80,6 +82,7 @@ public class SupermarketReceipt
 {
     private readonly Catalog _catalog = new();
     private readonly List<string> _products = [];
+    private readonly List<string> _discounts = [];
 
     public void AddProductToCar(string product)
     {
@@ -87,38 +90,83 @@ public class SupermarketReceipt
         _products.Add(product);
     }
 
+    public void AddDiscounts(List<string> discounts)
+    {
+        _discounts.AddRange(discounts);
+    }
+
     public decimal GetReceipt()
     {
-        var productAmount = _products.Count(product => product == "Cepillo");
+        var discount = 0m;
         
-        if(productAmount == 3)
-            _products.Remove("Cepillo");
+        if(_discounts.Any(product => product == nameof(ProductName.Cepillo)) 
+            && _products.Count(product => product == nameof(ProductName.Cepillo)) == 3)
+            discount = DiscountAmount(1);
+        
+        if(_discounts.Any(product => product == nameof(ProductName.Manzana))
+            && _products.Any(product => product == nameof(ProductName.Manzana)))
+            discount = DiscountPercentage(0.2m); 
 
-        return _products.Sum(product => _catalog.GetPriceProduct(product));
+        return _products.Sum(product => _catalog.GetPriceProduct(product)) - discount;
+    }
+
+    private decimal DiscountAmount(int freeAmount)
+    {
+        return _products
+            .Where(product => product == nameof(ProductName.Cepillo))
+            .Take(freeAmount)
+            .Sum(product => _catalog.GetPriceProduct(product));
+    }
+
+    private decimal DiscountPercentage(decimal percentage)
+    {
+        return _products
+            .Where(product => product == nameof(ProductName.Manzana))
+            .Sum(product => _catalog.GetPriceProduct(product)) * percentage;
     }
 }
 
-
 public class Catalog
 {
-    private Dictionary<string, decimal> _products = new()
+    private readonly List<Product> _products;
+    public Catalog()
     {
-        {"Cepillo", 0.99m},
-        {"Manzana", 1.99m},
-        {"Arroz", 2.49m},
-        {"TuboPastaDientes", 1.79m},
-        {"TomateCherry",  0.69m }
-    };
+        var storeProducts =  new StoreProducts();
+        _products = storeProducts.GetProducs();
+    }
 
     public void ExistsProductInCatalog(string product)
     {
-        if(_products.All(productInCatalog => productInCatalog.Key != product))
+        if(_products.All(productInCatalog => productInCatalog.Name != product))
             throw new Exception($"No existe un producto con el nombre {product}");
     }
 
     public decimal GetPriceProduct(string product)
     {
         ExistsProductInCatalog(product);
-        return _products.FirstOrDefault(productInCatalog => productInCatalog.Key == product).Value;
+        return _products.FirstOrDefault(productInCatalog => productInCatalog.Name == product)!.Price;
     }
+}
+
+public record Product(string Name, decimal Price);
+
+public enum ProductName
+{
+    Cepillo,
+    Manzana,
+    Arroz,
+    TuboPastaDientes,
+    TomateCherry
+}
+
+public class StoreProducts
+{
+    public List<Product> GetProducs() =>
+    [
+        new(nameof(ProductName.Cepillo), 0.99m),
+        new(nameof(ProductName.Manzana), 1.99m),
+        new(nameof(ProductName.Arroz), 2.49m),
+        new(nameof(ProductName.TuboPastaDientes), 1.79m),
+        new(nameof(ProductName.TomateCherry), 0.69m)
+    ];
 }
