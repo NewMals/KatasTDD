b49295649bf1f31d367d083bbe02ed12b3b3fa95
NewMals <newmals313@gmail.com>
2025-11-16 19:23:34 -0500

    🔴 Si_ElTextoParaAjustarEsVacio_Entonces_Debe_DevolverVacio

--- Katas/Kata.WordWrap/WordWrapTest.cs
+++ Katas/Kata.WordWrap/WordWrapTest.cs
@@ -5,6 +5,17 @@ namespace Kata.WordWrap;
 public class WordWrapTest
 {
     
+    [Fact]
+    public void Si_ElTextoParaAjustarEsVacio_Entonces_Debe_DevolverVacio()
+    {
+        var result = Wrap("", 1);
 
+        result.Should().Be("");
+    }
+    
+    private static string Wrap(string text, int col)
+    {
+        throw new NotImplementedException();
+    }
     
 }
