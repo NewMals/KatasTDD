2590466b3bf6176e1f2bf0f2c1b5d1d67d85cd52
NewMals <newmals313@gmail.com>
2025-11-16 19:29:04 -0500

    🔴 Si_ElTextoParaAjustarEsThis_Y_LaCantidadDeLasColumnaEs10_Debe_DevolverThis

--- Katas/Kata.WordWrap/WordWrapTest.cs
+++ Katas/Kata.WordWrap/WordWrapTest.cs
@@ -13,6 +13,14 @@ public class WordWrapTest
         result.Should().Be("");
     }
     
+    [Fact]
+    public void Si_ElTextoParaAjustarEsThis_Y_LaCantidadDeLasColumnaEs10_Debe_DevolverThis()
+    {
+        var result = Wrap("this", 10);
+
+        result.Should().Be("this");
+    }  
+    
     private static string Wrap(string text, int col)
     {
         return "";
